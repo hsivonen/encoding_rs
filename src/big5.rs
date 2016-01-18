24af2766c955ab994d3d877fb3bf46e9862f0cb7
Henri Sivonen <hsivonen@hsivonen.fi>
2016-01-18 12:55:21 +0200

    Macroize the Big5 decode function.

--- src/big5.rs
+++ src/big5.rs
@@ -12,6 +12,88 @@ use DecoderResult;
 use handles::*;
 use data::*;
 
+macro_rules! decoder_function {
+    ($preamble:block,
+     $eof:block,
+     $body:block,
+     $slf:ident,
+     $src_consumed:ident,
+     $dest:ident,
+     $b:ident,
+     $destination_handle:ident,
+     $unread_handle:ident,
+     $name:ident,
+     $code_unit:ty,
+     $dest_struct:ident) => (
+    fn $name(&mut $slf,
+             src: &[u8],
+             dst: &mut [$code_unit],
+             last: bool)
+             -> (DecoderResult, usize, usize) {
+        let mut source = ByteSource::new(src);
+        let mut $dest = $dest_struct::new(dst);
+        loop {
+            {
+                // Start non-boilerplate
+                $preamble
+                // End non-boilerplate
+            }
+            loop {
+                match source.check_available() {
+                    Space::Full($src_consumed) => {
+                        if last {
+                            // Start non-boilerplate
+                            $eof
+                            // End non-boilerplate
+                        }
+                        return (DecoderResult::InputEmpty, $src_consumed, $dest.written());
+                    }
+                    Space::Available(source_handle) => {
+                        match $dest.check_space_big5() {
+                            Space::Full(dst_written) => {
+                                return (DecoderResult::OutputFull,
+                                        source_handle.consumed(),
+                                        dst_written);
+                            }
+                            Space::Available($destination_handle) => {
+                                let ($b, $unread_handle) = source_handle.read();
+                                // Start non-boilerplate
+                                $body
+                                // End non-boilerplate
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    });
+}
+
+macro_rules! decoder_functions {
+    ($preamble:block,
+     $eof:block,
+     $body:block,
+     $slf:ident,
+     $src_consumed:ident,
+     $dest:ident,
+     $b:ident,
+     $destination_handle:ident,
+     $unread_handle:ident) => (
+    decoder_function!($preamble,
+                      $eof,
+                      $body,
+                      $slf,
+                      $src_consumed,
+                      $dest,
+                      $b,
+                      $destination_handle,
+                      $unread_handle,
+                      decode_to_utf16,
+                      u16,
+                      Utf16Destination);
+    );
+}
+
 pub struct Big5Decoder {
     lead: u8,
 }
@@ -49,127 +131,98 @@ impl Decoder for Big5Decoder {
         3 * self.plus_one_if_lead(byte_length) // XXX tail
     }
 
-    fn decode_to_utf16(&mut self,
-                       src: &[u8],
-                       dst: &mut [u16],
-                       last: bool)
-                       -> (DecoderResult, usize, usize) {
-        let mut source = ByteSource::new(src);
-        let mut dest = Utf16Destination::new(dst);
-        loop {
-            {
-                // Start non-boilerplate
-                // TODO: ISO-2022-JP
-                // End non-boilerplate
-            }
-            loop {
-                match source.check_available() {
-                    Space::Full(src_consumed) => {
-                        if last {
-                            // Start non-boilerplate
-                            if self.lead != 0 {
-                                self.lead = 0;
-                                return (DecoderResult::Malformed(1), src_consumed, dest.written());
-                            }
-                            // End non-boilerplate
-                        }
-                        return (DecoderResult::InputEmpty, src_consumed, dest.written());
-                    }
-                    Space::Available(source_handle) => {
-                        match dest.check_space_big5() {
-                            Space::Full(dst_written) => {
-                                return (DecoderResult::OutputFull,
-                                        source_handle.consumed(),
-                                        dst_written);
-                            }
-                            Space::Available(destination_handle) => {
-                                let (b, unread_handle) = source_handle.read();
-                                // Start non-boilerplate
-                                if self.lead == 0 {
-                                    if b <= 0x7f {
-                                        // TODO optimize ASCII run
-                                        destination_handle.write_ascii(b);
-                                        continue;
-                                    }
-                                    if b >= 0x81 && b <= 0xFE {
-                                        self.lead = b;
-                                        continue;
-                                    }
-                                    return (DecoderResult::Malformed(1),
-                                            unread_handle.consumed(),
-                                            destination_handle.written());
-                                }
-                                let lead = self.lead as usize;
-                                self.lead = 0;
-                                let offset = if b < 0x7F {
-                                    0x40usize
-                                } else {
-                                    0x62usize
-                                };
-                                if (b >= 0x40 && b <= 0x7E) || (b >= 0xA1 && b <= 0xFE) {
-                                    let pointer = (lead - 0x81usize) * 157usize +
-                                                  (b as usize - offset);
-                                    match pointer {
-                                        1133 => {
-                                            destination_handle.write_big5_combination(0x00CAu16,
-                                                                                      0x0304u16);
-                                            continue;
-                                        }
-                                        1135 => {
-                                            destination_handle.write_big5_combination(0x00CAu16,
-                                                                                      0x030Cu16);
-                                            continue;
-                                        }
-                                        1164 => {
-                                            destination_handle.write_big5_combination(0x00EAu16,
-                                                                                      0x0304u16);
-                                            continue;
-                                        }
-                                        1166 => {
-                                            destination_handle.write_big5_combination(0x00EAu16,
-                                                                                      0x030Cu16);
-                                            continue;
-                                        }
-                                        _ => {
-                                            let low_bits = big5_low_bits(pointer);
-                                            if low_bits == 0 {
-                                                if b <= 0x7F {
-                                                    return (DecoderResult::Malformed(1),
-                                                            unread_handle.unread(),
-                                                            destination_handle.written());
-                                                }
-                                                return (DecoderResult::Malformed(2),
-                                                        unread_handle.consumed(),
-                                                        destination_handle.written());
-                                            }
-                                            if big5_is_astral(pointer) {
-                                                destination_handle.write_astral(low_bits as u32 |
-                                                                                0x20000u32);
-                                                continue;
-                                            }
-                                            destination_handle.write_bmp_excl_ascii(low_bits);
-                                            continue;
-                                        }
-                                    }
-                                }
-                                // pointer is null
-                                if b <= 0x7F {
-                                    return (DecoderResult::Malformed(1),
-                                            unread_handle.unread(),
-                                            destination_handle.written());
-                                }
-                                return (DecoderResult::Malformed(2),
-                                        unread_handle.consumed(),
-                                        destination_handle.written());
+    decoder_functions!({},
+                       {
+                           if self.lead != 0 {
+                               self.lead = 0;
+                               return (DecoderResult::Malformed(1), src_consumed, dest.written());
+                           }
+                       },
+                       {
+                           if self.lead == 0 {
+                               if b <= 0x7f {
+                                   // TODO optimize ASCII run
+                                   destination_handle.write_ascii(b);
+                                   continue;
+                               }
+                               if b >= 0x81 && b <= 0xFE {
+                                   self.lead = b;
+                                   continue;
+                               }
+                               return (DecoderResult::Malformed(1),
+                                       unread_handle.consumed(),
+                                       destination_handle.written());
+                           }
+                           let lead = self.lead as usize;
+                           self.lead = 0;
+                           let offset = if b < 0x7F {
+                               0x40usize
+                           } else {
+                               0x62usize
+                           };
+                           if (b >= 0x40 && b <= 0x7E) || (b >= 0xA1 && b <= 0xFE) {
+                               let pointer = (lead - 0x81usize) * 157usize + (b as usize - offset);
+                               match pointer {
+                                   1133 => {
+                                       destination_handle.write_big5_combination(0x00CAu16,
+                                                                                 0x0304u16);
+                                       continue;
+                                   }
+                                   1135 => {
+                                       destination_handle.write_big5_combination(0x00CAu16,
+                                                                                 0x030Cu16);
+                                       continue;
+                                   }
+                                   1164 => {
+                                       destination_handle.write_big5_combination(0x00EAu16,
+                                                                                 0x0304u16);
+                                       continue;
+                                   }
+                                   1166 => {
+                                       destination_handle.write_big5_combination(0x00EAu16,
+                                                                                 0x030Cu16);
+                                       continue;
+                                   }
+                                   _ => {
+                                       let low_bits = big5_low_bits(pointer);
+                                       if low_bits == 0 {
+                                           if b <= 0x7F {
+                                               return (DecoderResult::Malformed(1),
+                                                       unread_handle.unread(),
+                                                       destination_handle.written());
+                                           }
+                                           return (DecoderResult::Malformed(2),
+                                                   unread_handle.consumed(),
+                                                   destination_handle.written());
+                                       }
+                                       if big5_is_astral(pointer) {
+                                           destination_handle.write_astral(low_bits as u32 |
+                                                                           0x20000u32);
+                                           continue;
+                                       }
+                                       destination_handle.write_bmp_excl_ascii(low_bits);
+                                       continue;
+                                   }
+                               }
+                           }
+                           // pointer is null
+                           if b <= 0x7F {
+                               return (DecoderResult::Malformed(1),
+                                       unread_handle.unread(),
+                                       destination_handle.written());
+                           }
+                           return (DecoderResult::Malformed(2),
+                                   unread_handle.consumed(),
+                                   destination_handle.written());
 
-                                // End non-boilerplate
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+
+                       },
+                       self,
+                       src_consumed,
+                       dest,
+                       b,
+                       destination_handle,
+                       unread_handle);
 
     fn decode_to_utf8(&mut self,
                       src: &[u8],
