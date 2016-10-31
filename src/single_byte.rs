a8e2a7add93a4856758ece70429753ff8ad2b3ab
Henri Sivonen <hsivonen@hsivonen.fi>
2016-10-31 09:35:13 +0200

    Map 0xCA to U+05BA in windows-1255 per whatwg/encoding#73.

--- src/single_byte.rs
+++ src/single_byte.rs
@@ -124,6 +124,12 @@ mod tests {
     use super::super::testing::*;
     use super::super::*;
 
+    #[test]
+    fn test_windows_1255_ca() {
+        decode(WINDOWS_1255, b"\xCA", "\u{05BA}");
+        encode(WINDOWS_1255, "\u{05BA}", b"\xCA");
+    }
+
     pub const HIGH_BYTES: &'static [u8; 128] = &[0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87,
                                                  0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F,
                                                  0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97,
