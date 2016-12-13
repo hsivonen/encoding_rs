8502ec7ca16b60b48fb8827df804f289636e411e
Henri Sivonen <hsivonen@hsivonen.fi>
2016-12-13 10:07:53 +0200

    Change variable name from `string` to `cow` in an example.

--- src/lib.rs
+++ src/lib.rs
@@ -40,8 +40,8 @@
 //! let expectation = "\u{30CF}\u{30ED}\u{30FC}\u{30FB}\u{30EF}\u{30FC}\u{30EB}\u{30C9}";
 //! let bytes = b"\x83n\x83\x8D\x81[\x81E\x83\x8F\x81[\x83\x8B\x83h";
 //!
-//! let (string, encoding_used, had_errors) = SHIFT_JIS.decode(bytes);
-//! assert_eq!(&string[..], expectation);
+//! let (cow, encoding_used, had_errors) = SHIFT_JIS.decode(bytes);
+//! assert_eq!(&cow[..], expectation);
 //! assert_eq!(encoding_used, SHIFT_JIS);
 //! assert!(!had_errors);
 //! ```
