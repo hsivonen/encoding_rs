5717a6adb26ad11a6cec300d52d915ce2820f27e
Simon Sapin <simon.sapin@exyr.org>
2017-03-09 11:35:47 +0100

    Refactor to reduce and document unsafe blocks

--- src/stream.rs
+++ src/stream.rs
@@ -18,43 +18,50 @@ pub use std::io::Read as ReadBytes;
 pub use std::fmt::Write as WriteUnicode;
 
 pub trait ReadUnicode {
-    fn read(&mut self, buf: &mut str) -> io::Result<usize>;
+    fn read(&mut self, dst: &mut str) -> io::Result<usize>;
 }
 
 impl<'a, S: ReadUnicode> ReadUnicode for &'a mut S {
-    fn read(&mut self, buf: &mut str) -> io::Result<usize> {
-        (**self).read(buf)
+    fn read(&mut self, dst: &mut str) -> io::Result<usize> {
+        (**self).read(dst)
     }
 }
 
 /// Similar to `std::io::Read for &'a [u8]`.
 impl<'a> ReadUnicode for &'a str {
-    fn read(&mut self, buf: &mut str) -> io::Result<usize> {
-        let self_bytes = self.as_bytes();
-        let bytes_to_copy = if buf.len() >= self.len() {
-            self.len()
-        } else {
-            let mut i = buf.len() - 1;
-            while (self_bytes[i] & 0xC0) == 0x80 {
-                i -= 1;
-            }
-            i
-        };
-        let buf_bytes = unsafe { mem::transmute::<&mut str, &mut [u8]>(buf) };
-        let (to_copy_from, remaining_self) = self.split_at(bytes_to_copy);
-        let (to_copy_into, remaining_buf) = buf_bytes.split_at_mut(bytes_to_copy);
-        to_copy_into.copy_from_slice(to_copy_from.as_bytes());
-        for byte in remaining_buf {
-            if (*byte & 0xC0) == 0x80 {
-                *byte = 0
-            } else {
-                break
-            }
-        }
+    fn read(&mut self, dst: &mut str) -> io::Result<usize> {
+        // Unsafe: copy_maximum_utf8_prefix carefully preserves UTF-8 wel-formedness.
+        let dst_bytes = unsafe { mem::transmute::<&mut str, &mut [u8]>(dst) };
+        let (bytes_copied, remaining_self) = copy_maximum_utf8_prefix(*self, dst_bytes);
         *self = remaining_self;
-        Ok(bytes_to_copy)
+        Ok(bytes_copied)
     }
 }
+
+fn copy_maximum_utf8_prefix<'a>(src: &'a str, dst: &mut [u8]) -> (usize, &'a str) {
+    let src_bytes = src.as_bytes();
+    let bytes_to_copy = if dst.len() >= src.len() {
+        src.len()
+    } else {
+        let mut i = dst.len() - 1;
+        while (src_bytes[i] & 0xC0) == 0x80 {
+            i -= 1;
+        }
+        i
+    };
+    let (to_copy_from, remaining_src) = src.split_at(bytes_to_copy);
+    let (to_copy_into, remaining_dst) = dst.split_at_mut(bytes_to_copy);
+    to_copy_into.copy_from_slice(to_copy_from.as_bytes());
+    for byte in remaining_dst {
+        if (*byte & 0xC0) == 0x80 {
+            *byte = 0
+        } else {
+            break
+        }
+    }
+    (bytes_to_copy, remaining_src)
+}
+
 
 // FIXME: with vs without replacement
 
@@ -80,6 +87,8 @@ impl<Stream: ReadBytes, Buffer: AsMut<[u8]>> ReadDecoder<Stream, Buffer> {
 
 impl<Stream: ReadBytes, Buffer: AsMut<[u8]>> ReadUnicode for ReadDecoder<Stream, Buffer> {
     fn read(&mut self, dst: &mut str) -> io::Result<usize> {
+        // Unsafe: Like Decoder::decode_to_utf8, <ReadDecoder as ReadBytes>::read’s contract
+        // is to preserve UTF-8 well-formedness.
         let dst = unsafe { mem::transmute::<&mut str, &mut [u8]>(dst) };
         ReadBytes::read(self, dst)
     }
@@ -90,13 +99,10 @@ impl<Stream: ReadBytes, Buffer: AsMut<[u8]>> ReadBytes for ReadDecoder<Stream, B
         let buffer = self.buffer.as_mut();
         if self.unused_buffer_slice.end > self.unused_buffer_slice.start {
             let buffer_slice = &buffer[self.unused_buffer_slice.clone()];
-            let mut buffer_slice = unsafe { str::from_utf8_unchecked(buffer_slice) };
-
-            // This transmute is a lie: `dst` might not be UTF-8 at all.
-            // But we know that `<&str as ReadUnicode>::read` doesn’t rely on it being UTF-8.
-            let dst = unsafe { mem::transmute::<&mut [u8], &mut str>(dst) };
-
-            let bytes_copied = ReadUnicode::read(&mut buffer_slice, dst)?;
+            // Unsafe: this slice was written by Decoder::decode_to_utf8,
+            // whose contract is to make it well-formed UTF-8.
+            let buffer_slice = unsafe { str::from_utf8_unchecked(buffer_slice) };
+            let (bytes_copied, _) = copy_maximum_utf8_prefix(buffer_slice, dst);
             self.unused_buffer_slice.start += bytes_copied;
             Ok(bytes_copied)
         } else if !self.reached_stream_eof {
