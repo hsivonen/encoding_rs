229d34374bde30c8b9603a03654d7c308ade5df1
AuroraAeon <thomaspeng2021@gmail.com>
2026-04-23 22:24:29 +0800

    Add license header to streaming test file

--- tests/streaming_empty_chunks.rs
+++ tests/streaming_empty_chunks.rs
@@ -1,3 +1,5 @@
+// Any copyright to the test code below this comment is dedicated to the
+// Public Domain. http://creativecommons.org/publicdomain/zero/1.0/
 use encoding_rs::*;
 
 fn decode_chunks_to_utf8(encoding: &'static Encoding, chunks: &[&[u8]]) -> (String, bool) {
